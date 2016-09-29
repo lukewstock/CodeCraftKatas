957ceeae1db755058af34bb214e7b9a465c2dfab
lukewstock <lukewstock@gmail.com>
2016-09-29 13:34:45 +0100

    Initial check in of the Tic Tac Tioe Kata

---
+++ TicTacToeKata/TicTacToeKata.Tests/TicTacToeShould.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TicTacToeKata.Tests
+{
+    public class TicTacToeShould
+    {
+        [Test]
+        public void BeEqual_GivenTwoNewGames()
+        {
+            var firstNewGame = new TicTacToe();
+            var secondNewGame = new TicTacToe();
+
+            firstNewGame.Should().Be(secondNewGame);
+        }
+
+        [Test]
+        public void NotBeEqual_GivenOneNewGameAndOneGameWithTopLeftPlayed()
+        {
+            var newGame = new TicTacToe();
+            var playedGame = new TicTacToe();
+            playedGame.PlayTopLeft();
+
+            newGame.Should().NotBe(playedGame);
+        }
+
+        [Test]
+        public void NotBeEqual_GivenOneNewGameAndOneGameWithTopRightPlayed()
+        {
+            var newGame = new TicTacToe();
+            var topRightGame = new TicTacToe();
+            topRightGame.PlayTopRight();
+
+            newGame.Should().NotBe(topRightGame);
+        }
+
+        [Test]
+        public void NotBeEqual_GivenOneTopLeftGameAndOneTopRightGame()
+        {
+            var topLeftGame = new TicTacToe();
+            topLeftGame.PlayTopLeft();
+            var topRightGame = new TicTacToe();
+            topRightGame.PlayTopRight();
+
+            topLeftGame.Should().NotBe(topRightGame);
+
+
+        }
+    }
+}
---
+++ TicTacToeKata/TicTacToeKata/TicTacToe.cs
@@ -0,0 +1,49 @@
+﻿namespace TicTacToeKata
+{
+    public class TicTacToe
+    {
+        private bool _played;
+
+        protected bool Equals(TicTacToe other)
+        {
+            return _played.Equals(other._played);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((TicTacToe) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _played.GetHashCode();
+        }
+
+        public static bool operator ==(TicTacToe left, TicTacToe right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(TicTacToe left, TicTacToe right)
+        {
+            return !Equals(left, right);
+        }
+
+        public void PlayTopLeft()
+        {
+            this._played = true;
+        }
+
+        public void PlayTopRight()
+        {
+        }
+
+        public void PlayTopMiddle()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
