77c0d7563c5fd39b95fc4052600fbbe94268ea45
lukewstock <lukewstock@gmail.com>
2016-10-27 16:33:51 +0100

    test that position is returned

--- MarsRoverKata/MarsRoverTests/MarsRoverShould.cs
+++ MarsRoverKata/MarsRoverTests/MarsRoverShould.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace MarsRoverTests
@@ -10,7 +11,18 @@ namespace MarsRoverTests
     [TestFixture]
     public class MarsRoverShould
     {
-        
+        [Test]
+        public void a()
+        {
+            new MarsRover().GetP().Should().Be("1 1 N");
+        }
+    }
 
+    public class MarsRover
+    {
+        public string GetP()
+        {
+            return "1 1 N";
+        }
     }
 }
