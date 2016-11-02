9e1d7db4aa6852ab517c05efc37e552a9c575d71
lukewstock <lukewstock@gmail.com>
2016-11-02 11:43:01 +0000

    Refactor tests

--- MarsRoverKata/MarsRoverKata/MarsRover.cs
+++ MarsRoverKata/MarsRoverKata/MarsRover.cs
@@ -10,11 +10,6 @@
             
         }
 
-        public MarsRover()
-        {
-            throw new System.NotImplementedException();
-        }
-
         public string GetCurrentPosition()
         {
             return _startingPosition;
--- MarsRoverKata/MarsRoverTests/MarsRoverShould.cs
+++ MarsRoverKata/MarsRoverTests/MarsRoverShould.cs
@@ -8,26 +8,21 @@ namespace MarsRoverTests
     public class MarsRoverShould
     {
         [Test]
-        public void ReturnPosition()
-        {
-            var marsRover = new MarsRover("1 1 N");
-            var expectedPosition = "1 1 N";
-
-            var position = marsRover.GetCurrentPosition();
-
-            position.Should().Be(expectedPosition);
-        }
-
-        [Test]
         public void ReturnStartingPositionWhenNoInstructionsHaveBeenProcessed()
         {
-            new MarsRover("1 2 N").GetCurrentPosition().Should().Be("1 2 N");
+            const string startingPosition = "1 2 N";
+            var marsRover = new MarsRover(startingPosition);
+
+            var currentPosition = marsRover.GetCurrentPosition();
+
+            currentPosition.Should().Be(startingPosition);
         }
 
         [Test]
-        public void x()
+        public void ReturnNewPostionWhenMoving()
         {
             var marsRover = new MarsRover("1 1 N");
+
             marsRover.Move("R");
 
             marsRover.GetCurrentPosition().Should().Be("1 1 E");
