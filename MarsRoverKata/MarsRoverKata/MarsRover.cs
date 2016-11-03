f576ba16fca3dbff0b7d4c5b5a1838e0efd19f65
lukewstock <lukewstock@gmail.com>
2016-11-03 15:48:29 +0000

    refactor move method

--- MarsRoverKata/MarsRoverKata/MarsRover.cs
+++ MarsRoverKata/MarsRoverKata/MarsRover.cs
@@ -2,12 +2,14 @@
 {
     public class MarsRover
     {
+        private const string ROTATE_RIGHT = "R";
+        private const string ROTATE_LEFT = "L";
+        private const string MOVE_FORWARD = "M";
         private string _startingPosition;
 
         public MarsRover(string startingPosition)
         {
             _startingPosition = startingPosition;
-            
         }
 
         public string GetCurrentPosition()
@@ -17,51 +19,64 @@
 
         public void Move(string instructions)
         {
-            if (instructions == "R")
+            if (instructions == ROTATE_RIGHT)
             {
-                var newPosition = string.Empty;
-
-                if (_startingPosition == "1 1 S")
-                {
-                    newPosition = "1 1 W";
-                }
-
-                if (_startingPosition == "1 1 E")
-                {
-                    newPosition = "1 1 S";
-                }
-
-                if (_startingPosition == "1 1 N")
-                {
-                    newPosition = "1 1 E";
-                }
-
-                if (_startingPosition == "1 1 W")
-                {
-                    newPosition = "1 1 N";
-                }
-
-                _startingPosition = newPosition;
+                RotateRight();
             }
 
-            if (instructions == "L")
+            if (instructions == ROTATE_LEFT)
             {
-                _startingPosition = "1 1 W";
+                RotateLeft();
             }
 
-
-
-            if (instructions == "M")
+            if (instructions == MOVE_FORWARD)
             {
-                if (_startingPosition == "1 1 E")
-                {
-                    _startingPosition = "2 1 E";
-                }
-                else
-                {
-                    _startingPosition = "1 2 N";
-                }
+                MoveForwardOneSpace();
             }
+        }
+
+        private void MoveForwardOneSpace()
+        {
+            if (_startingPosition == "1 1 E")
+            {
+                _startingPosition = "2 1 E";
+            }
+            else
+            {
+                _startingPosition = "1 2 N";
+            }
+        }
+
+        private void RotateLeft()
+        {
+            _startingPosition = "1 1 W";
+        }
+
+        private void RotateRight()
+        {
+            var newPosition = string.Empty;
+
+            if (_startingPosition == "1 1 S")
+            {
+                newPosition = "1 1 W";
+            }
+
+            if (_startingPosition == "1 1 E")
+            {
+                newPosition = "1 1 S";
+            }
+
+            if (_startingPosition == "1 1 N")
+            {
+                newPosition = "1 1 E";
+            }
+
+            if (_startingPosition == "1 1 W")
+            {
+                newPosition = "1 1 N";
+            }
+
+            _startingPosition = newPosition;
         }
     }
 }
